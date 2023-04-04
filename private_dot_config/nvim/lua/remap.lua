71c5ef760282fe88e03b6252c65a7d3218806325
Artem Bliednov <artem@bliednov.com>
2023-04-04 22:03:50 +0200

    feat: add nvim config

---
+++ private_dot_config/nvim/lua/remap.lua
@@ -0,0 +1,24 @@
+-- [[ Basic Keymaps ]]
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', 'tn', vim.cmd.tabnew)
+vim.keymap.set('n', 'tc', vim.cmd.tabclose)
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
